2c49bc3fc698bfa125601853db0946e931954221
Andrew Kelley <akelley@backtrace.io>
2016-11-10 10:35:28 -0500

    extend testing to cover report send callback

--- test/request_report_object.js
+++ test/request_report_object.js
@@ -1,8 +1,9 @@
 var http = require('http');
+var assert = require('assert');
+var StreamSink = require('streamsink');
 var bt = require('../');
 var port = parseInt(process.argv[2], 10);
 var token = process.argv[3];
-var StreamSink = require('streamsink');
 
 bt.initialize({
   endpoint: "http://localhost:" + port,
@@ -21,13 +22,17 @@ server.listen(0, "localhost", function() {
     sink.on('finish', function() {
       report.addAttribute("endTime", new Date().getTime());
       report.setError(new Error("RIP"));
-      report.send();
-      response.end();
+      report.send(function(err) {
+        if (err) throw err;
+        response.statusCode = 500;
+        response.end();
+      });
     });
     request.pipe(sink);
   });
 
   http.get("http://localhost:" + port + "/path", function(resp) {
+    assert.strictEual(resp.statusCode, 500);
     server.close();
   });
 });
