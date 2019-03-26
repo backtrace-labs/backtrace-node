0d712acbacac6c3a86cc827f1831db203d83a7ee
Konrad Dysput <konrad.dysput@gmail.com>
2019-03-26 18:55:11 +0100

    Memorized attributes support + documentation

--- source/backtraceClient.ts
+++ source/backtraceClient.ts
@@ -6,7 +6,7 @@ import { BacktraceApi } from './backtraceApi';
  * Backtrace client
  */
 export class BacktraceClient {
-  private _memorizedAttributes!: object;
+  private _memorizedAttributes: object = {};
   private _backtraceApi: BacktraceApi;
   constructor(private options: BacktraceClientOptions) {
     options = options || new BacktraceClientOptions();
@@ -17,32 +17,68 @@ export class BacktraceClient {
     this.registerHandlers();
   }
 
+  /**
+   * Memorize selected values from application. 
+   * Memorized attributes will be available in your next Backtrace report. 
+   * Memorized attributes will be only available for one report.
+   * @param key attribute key
+   * @param value attribute value
+   */
   public memorize(key: string, value: any): void {
     (this._memorizedAttributes as any)[key] = value;
   }
 
+  /**
+   * Send report asynchronously to Backtrace
+   * @param arg report payload
+   * @param arg2 attributes
+   * @param arg3 file attachments paths
+   */
   public async reportAsync(
     arg: Error | string,
     arg2: object | undefined = {},
     arg3: string[] = []
   ): Promise<void> {
-    const attributes = { ...arg2, ...this.options.attributes };
+    const attributes = this.combineClientAttributes(arg2);
     const report = new BacktraceReport(arg, attributes, arg3);
     await this._backtraceApi.send(report);
   }
 
+  /**
+   * Send report synchronosuly to Backtrace
+   * @param error report payload
+   * @param reportAttributes attributes
+   * @param attachments file attachments paths
+   */
   public reportSync(
     error: Error | string,
     reportAttributes: object | undefined = {},
     attachments: string[] = []
   ): void {
-    const attributes = { ...reportAttributes, ...this.options.attributes };
+    const attributes = this.combineClientAttributes(reportAttributes);
     const report = new BacktraceReport(error, attributes, attachments);
     this._backtraceApi.send(report);
   }
 
+  private combineClientAttributes(attributes: object = {}): object {
+    if (!attributes) {
+      attributes = {};
+    }
+    return {
+      ...attributes,
+      ...this.options.attributes,
+      ...this.getMemorizedAttributes(),
+    };
+  }
+
+  private getMemorizedAttributes() {
+    const result = this._memorizedAttributes;
+    this._memorizedAttributes = {};
+    return result;
+  }
+
   private registerHandlers(): void {
-    if (!this.options.disableGlobalHandler) {
+    if (!!this.options.disableGlobalHandler) {
       this.registerGlobalHandler(
         !!this.options.allowMultipleUncaughtExceptionListeners
       );
