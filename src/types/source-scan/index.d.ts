32246da50366b989f9b06183b523b8c5b1ae004f
Konrad Dysput <konrad.dysput@gmail.com>
2019-04-23 16:57:15 +0200

    Library configuration for publication

--- src/types/source-scan/index.d.ts
+++
@@ -1,8 +0,0 @@
-declare module 'source-scan' {
-  export interface IScanFileOptions {
-    filePath: string;
-    startLine: number;
-    endLine: number;
-  }
-  export function scanFile(options: IScanFileOptions, callback: (err: Error, buff: Buffer) => void): void;
-}
